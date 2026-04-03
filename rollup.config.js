2a5d893bbabac87f1cb72c6bf3c6733225886d31
Amodio <jacques@boscq.fr>
2026-04-03 22:57:33 +0200

    Upgrade JS packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,26 @@
-// Import rollup plugins
-import html from '@web/rollup-plugin-html';
-//import {copy} from '@web/rollup-plugin-copy';
-import resolve from '@rollup/plugin-node-resolve';
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-//import summary from 'rollup-plugin-summary';
 
 export default {
+  input: 'src/index.html',
+
+  output: {
+    dir: 'build',
+    format: 'es',
+    sourcemap: true,
+  },
+
   plugins: [
-    // Entry point for application build; can specify a glob to build multiple
-    // HTML files for non-SPA app
     html({
-      input: 'src/index.html',
+      minify: true, // built-in HTML minification (safe replacement)
     }),
-    // Resolve bare module specifiers to relative paths
-    resolve(),
-    // Minify HTML template literals
-    minifyHTML(),
-    // Minify JS
+
+    nodeResolve(),
+
     terser({
       ecma: 2021,
       module: true,
-      warnings: true,
     }),
-    // Print bundle summary
-    //summary(),
-    // Optional: copy any static assets to build directory
-    //copy({
-    //  patterns: ['images/**/*'],
-    //}),
   ],
-  output: {
-    dir: 'build',
-  },
-  preserveEntrySignatures: 'strict',
 };
